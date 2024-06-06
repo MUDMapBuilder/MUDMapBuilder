b2a0fe7c6024cee7de7575c0b18c3e64c61e6977
Roman Shapiro <eleroman@mail.ru>
2024-06-06 19:21:39 +0700

    Added import utility

---
+++ MUDMapBuilder.Import.Console/Program.cs
@@ -0,0 +1,148 @@
+﻿using MUDMapBuilder.Import.Diku;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MUDMapBuilder.Import
+{
+	internal class Program
+	{
+		private const string OutputFolder = "output";
+
+		static void Process(string folder)
+		{
+			var areas = new List<MMBArea>();
+			/*			var folder = @"D:\Projects\chaos\Crimson-2-MUD\prod\lib";
+
+						var zonesTable = File.ReadAllText(Path.Combine(folder, "zones.tbl"));
+						var lines = zonesTable.Split('\n');
+						foreach (var line in lines)
+						{
+							var s = line.Trim();
+							if (s == "$~")
+							{
+								break;
+							}
+
+							var parts = s.Split(' ');
+
+							var path = Path.Combine(folder, "areas");
+							path = Path.Combine(path, parts[0]);
+							path = Path.ChangeExtension(path, "wld");
+
+							var area = Importer.ProcessFile(path);
+							areas.Add(area);
+
+						}*/
+
+			var files = Directory.EnumerateFiles(folder, "*.wld", SearchOption.AllDirectories);
+			foreach (var path in files)
+			{
+				var area = Importer.ProcessFile(path);
+				areas.Add(area);
+			}
+
+			// Build complete dictionary of rooms
+			var allRooms = new Dictionary<int, MMBRoom>();
+			foreach (var area in areas)
+			{
+				foreach (var room in area.Rooms)
+				{
+					if (allRooms.ContainsKey(room.Id))
+					{
+						throw new Exception($"Dublicate room id: {room.Id}");
+					}
+
+					var areaExit = room.Clone();
+					areaExit.Name = $"To {area.Name}";
+					areaExit.IsExitToOtherArea = true;
+
+					allRooms[room.Id] = areaExit;
+				}
+			}
+
+			// Now add areas exits
+			foreach (var area in areas)
+			{
+				var areaExits = new Dictionary<int, MMBRoom>();
+				foreach (var room in area.Rooms)
+				{
+					foreach (var exit in room.Connections)
+					{
+						MMBRoom inAreaRoom;
+						inAreaRoom = (from r in area.Rooms where r.Id == exit.Value.RoomId select r).FirstOrDefault();
+						if (inAreaRoom != null)
+						{
+							continue;
+						}
+
+						areaExits[exit.Value.RoomId] = allRooms[exit.Value.RoomId];
+					}
+				}
+
+				foreach (var pair in areaExits)
+				{
+					area.Add(pair.Value);
+				}
+			}
+
+			if (Directory.Exists(OutputFolder))
+			{
+				Directory.Delete(OutputFolder, true);
+
+			}
+
+			Directory.CreateDirectory(OutputFolder);
+
+			// Save all areas and generate conversion script
+			var sb = new StringBuilder();
+			foreach (var area in areas)
+			{
+				var fileName = $"output/{area.Name}.json";
+
+				if (fileName.StartsWith("Adria"))
+				{
+					continue;
+				}
+
+				Console.WriteLine($"Saving {fileName}...");
+
+				var project = new MMBProject(area, new BuildOptions());
+
+				/*				// Copy build options
+								var oldData = File.ReadAllText(Path.Combine(@"D:\Temp\MUDMapBuilder.0.1.0\circle_Areas", fileName));
+								var oldProject = MMBProject.Parse(oldData);
+
+								oldProject.BuildOptions.CopyTo(project.BuildOptions);*/
+
+				var data = project.ToJson();
+				File.WriteAllText(fileName, data);
+
+				var pngFileName = Path.ChangeExtension(fileName, "png");
+				sb.AppendLine($"mmb \"circle_Areas\\{fileName}\" \"circle_Maps\\{pngFileName}\"");
+			}
+
+			File.WriteAllText("convertTba.bat", sb.ToString());
+		}
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (args.Length < 1)
+				{
+					Console.WriteLine("Usage: mmb-import <inputFolder>");
+					return;
+				}
+
+				Process(args[0]);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+	}
+}
---
+++ MUDMapBuilder.Import.Diku/Importer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MUDMapBuilder.Import.Diku
+{
+	public static class Importer
+	{
+		public static void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		private static MMBArea ProcessMMBArea(Stream stream)
+		{
+			var vnum = int.Parse(stream.ReadId());
+			var credits = stream.ReadDikuString();
+			var name = stream.ReadDikuString();
+
+			return new MMBArea
+			{
+				Name = name.Replace('/', '_')
+			};
+		}
+
+		private static MMBRoom[] ProcessMMBRooms(Stream stream)
+		{
+			var result = new List<MMBRoom>();
+			while (!stream.EndOfStream())
+			{
+				var vnum = int.Parse(stream.ReadId());
+				var name = stream.ReadDikuString();
+				Log($"Processing room {name} (#{vnum})...");
+
+				var room = new MMBRoom(vnum, name, false);
+
+				var desc = stream.ReadDikuString();
+				stream.ReadNumber(); // MMBArea Number
+
+				var line = stream.ReadLine();
+
+				result.Add(room);
+				char c;
+				while (!stream.EndOfStream())
+				{
+					c = stream.ReadSpacedLetter();
+
+					if (c == 'S')
+					{
+						// End of room
+						break;
+					}
+					else if (c == 'H')
+					{
+						var healRate = stream.ReadNumber();
+					}
+					else if (c == 'M')
+					{
+						var manaRate = stream.ReadNumber();
+					}
+					else if (c == 'C')
+					{
+						string clan = stream.ReadDikuString();
+					}
+					else if (c == 'D')
+					{
+						var direction = (MMBDirection)stream.ReadNumber();
+						var exitDesc = stream.ReadDikuString();
+						var keyword = stream.ReadDikuString();
+
+						var locks = stream.ReadNumber();
+						var keyVNum = stream.ReadNumber();
+						var targetVnum = stream.ReadNumber();
+						if (targetVnum != -1)
+						{
+							room.Connections[direction] = new MMBRoomConnection(direction, targetVnum);
+						}
+					}
+					else if (c == 'E')
+					{
+						var extraKeyword = stream.ReadDikuString();
+						var extraDescription = stream.ReadDikuString();
+					}
+					else if (c == 'O')
+					{
+						var owner = stream.ReadDikuString();
+					}
+					else
+					{
+						throw new Exception($"Unknown room command '{c}'");
+					}
+				}
+
+				var doRun = true;
+				while (!stream.EndOfStream() && doRun)
+				{
+					c = stream.ReadSpacedLetter();
+					switch (c)
+					{
+						case '$':
+							goto finish;
+						case 'T':
+							var triggerId = stream.ReadNumber();
+							break;
+						default:
+							stream.Seek(-1, SeekOrigin.Current);
+							doRun = false;
+							break;
+					}
+				}
+			}
+		finish:;
+			return result.ToArray();
+		}
+
+		public static MMBArea ProcessFile(string areaFile)
+		{
+			Log($"Processing {areaFile}...");
+
+			// var zonPath = Path.ChangeExtension(areaFile, "zon");
+			var zonPath = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(areaFile)), "zon");
+			zonPath = Path.Combine(zonPath, Path.ChangeExtension(Path.GetFileName(areaFile), "zon"));
+
+			MMBArea result;
+
+			if (File.Exists(zonPath))
+			{
+				Log($"Getting area name from {zonPath}...");
+				using (var stream = File.OpenRead(zonPath))
+				{
+					result = ProcessMMBArea(stream);
+					Log($"Area name is '{result.Name}'");
+				}
+			}
+            else
+            {
+				result = new MMBArea
+				{
+					Name = Path.GetFileName(areaFile).Replace('/', '_')
+				};
+            }
+
+            using (var stream = File.OpenRead(areaFile))
+			{
+				var rooms = ProcessMMBRooms(stream);
+				foreach (var room in rooms)
+				{
+					result.Add(room);
+				}
+			}
+
+			return result;
+		}
+	}
+}

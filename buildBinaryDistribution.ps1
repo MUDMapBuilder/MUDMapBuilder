5702ec11bbf70b446fa9382e1d87a2ac44ff97ef
Roman Shapiro <eleroman@mail.ru>
2024-06-08 21:57:54 +0700

    Build Script fix

--- buildBinaryDistribution.ps1
+++ buildBinaryDistribution.ps1
@@ -6,11 +6,9 @@ Remove-Item -Recurse -Force "ZipPackage" -ErrorAction Ignore
 Remove-Item -Recurse -Force "MUDMapBuilder.$version" -ErrorAction Ignore
 
 New-Item -ItemType directory -Path "ZipPackage"
-New-Item -ItemType directory -Path "ZipPackage\ROM_Areas"
 
 # Copy-Item -Path files
 Copy-Item -Path "MUDMapBuilder.Console\bin\Release\net6.0\*" -Destination "ZipPackage" -Recurse
-Copy-Item -Path "ROM_Areas\*" -Destination "ZipPackage\ROM_Areas" -Recurse
 
 # Compress
 Rename-Item "ZipPackage" "MUDMapBuilder.$version"
